8176ce7ef0ad25d9fcb1347018e93d3a73cf7bee
joschjava <forenmaster@gmx.net>
2019-10-10 13:17:15 +0200

    Unignored Config file which now gets values from properties file

---
+++ src/main/java/datadownloader/Config.java
@@ -0,0 +1,40 @@
+package datadownloader;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+@Slf4j
+public class Config {
+    public static String API_KEY;
+    public static String USERNAME;
+    public static String SAVE_FILE;
+    public static String RESPONSE_LOG_FOLDER;
+    public static String CSV_DELIMITER;
+
+
+    public static void loadConfig() {
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                        .configure(params.properties()
+                                .setFileName("settings.properties"));
+        try {
+            Configuration config = builder.getConfiguration();
+            API_KEY = config.getString("apiKey");
+            USERNAME = config.getString("username");
+            SAVE_FILE = config.getString("saveFile");
+            RESPONSE_LOG_FOLDER = config.getString("reponseLogFolder");
+            CSV_DELIMITER = config.getString("csvDelimiter");
+        } catch (ConfigurationException e) {
+            log.error("loading of the configuration file failed:");
+            log.error(ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+}
